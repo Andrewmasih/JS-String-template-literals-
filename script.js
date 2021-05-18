3fdf30f5acf97016259b8c4d41539bc4f49df992
Andrew Masih <armasih@outlook.com>
2021-05-18 22:50:12 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let first = "firstString";
+let second = " and secondString";
+
+console.log (`concatenation is joining the two strings: 
+${first + second}`);
+
+/*join the 2 strings by putting the name of the string in the curley brackets preceded by the dollor sign: `${fisrt + second}` */
